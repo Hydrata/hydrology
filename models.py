a2e5cbc802f13e52309c21b1ac5c5d021d00f460
Dave Kennewell <david.kennewell@hydrata.com>
2024-04-01 10:28:50 +0400

    initial hydrology deploy test

--- models.py
+++ models.py
@@ -1,3 +1,11 @@
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.db.models import JSONField
+from django.contrib.postgres.indexes import GinIndex
+from datetime import datetime
+import pytz
+import csv
 
-# Create your models here.
+
+class TimeSeries(models.Model):
+    pass
