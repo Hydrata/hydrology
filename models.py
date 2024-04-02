51cee39374e4cba3e6c8ba7319ec247366174840
Dave Kennewell <david.kennewell@hydrata.com>
2024-04-02 20:02:54 +0400

    add TimeSeriesModel

--- models.py
+++ models.py
@@ -4,8 +4,43 @@ from django.db.models import JSONField
 from django.contrib.postgres.indexes import GinIndex
 from datetime import datetime
 import pytz
-import csv
 
 
 class TimeSeries(models.Model):
-    pass
+    data = JSONField()
+    timezone = models.CharField(max_length=50, default='UTC', choices=[(tz, tz) for tz in pytz.all_timezones])
+
+    def clean(self):
+        required_keys = {'ts', 'value'}
+        for data_index, data_point in enumerate(self.data):
+            if not required_keys.issubset(data_point):
+                raise ValidationError(f"The {required_keys} fields are required in each data point.")
+
+            timestamp_string = data_point.get('ts')
+            try:
+                datetime.fromisoformat(timestamp_string.rstrip('Z'))
+            except ValueError:
+                raise ValidationError("All timestamps must be in ISO 8601 format.")
+
+        if self.timezone not in pytz.all_timezones:
+            raise ValidationError("The 'timezone' field must contain a valid timezone.")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+    @property
+    def data_with_datetimes(self):
+        data_with_datetimes = []
+        tz = pytz.timezone(self.timezone)
+        for data_point in self.data:
+            data_point_copy = data_point.copy()
+            data_point_copy['ts'] = datetime.fromisoformat(data_point['ts'].rstrip('Z')).replace(tzinfo=tz)
+            data_with_datetimes.append(data_point_copy)
+        return data_with_datetimes
+
+    class Meta:
+        indexes = [
+            GinIndex(fields=['data']),
+            models.Index(fields=['timezone']),
+        ]
--- tests.py
+++ tests.py
@@ -1,3 +1,47 @@
-from django.test import TestCase
+import datetime
+import pytest
+import pytz
 
-# Create your tests here.
+from django.core.exceptions import ValidationError
+from hydrology.models import TimeSeries
+
+
+@pytest.mark.django_db
+class TestTimeSeriesModel:
+
+    def setup_method(self):
+        self.valid_time_data = [
+            {'ts': '2022-06-01T00:00:00', 'value': 10},
+            {'ts': '2022-07-01T00:00:00', 'value': 20},
+            {'ts': '2022-08-01T00:00:00', 'value': 30}
+        ]
+        self.invalid_time_data = [
+            {'ts': 'not a timestamp', 'value': 10},
+            {'ts': '2022-07-01T00:00:00', 'value': 20},
+            {'ts': '2022-08-01T00:00:00', 'value': 30}
+        ]
+
+    def test_model_creation_with_valid_data(self):
+        time_series = TimeSeries.objects.create(data=self.valid_time_data, timezone='UTC')
+        assert time_series is not None
+        assert time_series.data == self.valid_time_data
+        assert time_series.timezone == 'UTC'
+
+    def test_model_with_invalid_timestamps(self):
+        time_series = TimeSeries(data=self.invalid_time_data, timezone='UTC')
+        with pytest.raises(ValidationError):
+            time_series.save()
+
+    def test_model_with_invalid_timezone(self):
+        time_series = TimeSeries(data=self.valid_time_data, timezone='InvalidTimezone')
+        with pytest.raises(ValidationError):
+            time_series.save()
+
+    def test_data_with_datetimes(self):
+        time_series = TimeSeries.objects.create(data=self.valid_time_data, timezone='UTC')
+        expected_data = [
+            {'ts': datetime.datetime(2022, 6, 1, 0, 0, tzinfo=pytz.UTC), 'value': 10},
+            {'ts': datetime.datetime(2022, 7, 1, 0, 0, tzinfo=pytz.UTC), 'value': 20},
+            {'ts': datetime.datetime(2022, 8, 1, 0, 0, tzinfo=pytz.UTC), 'value': 30}
+        ]
+        assert time_series.data_with_datetimes == expected_data
